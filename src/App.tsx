2b7c6e754132b9543f33ccfb9fc21b861d3222a0
Joao <joaosperandio@alunos.utfpr.edu.br>
2021-12-08 09:25:31 -0300

    Add: React Router Dom support

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Home from './pages/home';
 import "antd/dist/antd.css";
 // import Localization from './pages/localization'
+import {BrowserRouter as Router, Route, Switch} from '../node_modules/react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Router>
+        <Route exact path="/" component={Home}/>
+      </Router>
     </div>
   );
 }
